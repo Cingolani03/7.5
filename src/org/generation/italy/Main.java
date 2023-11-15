f9db332871f5bebcd89ddbbf137d8eb9ce61c02f
Cingolani03 <gabrielecingolani61@gmail.com>
2023-11-15 12:19:41 +0100

    primo commit

---
+++ src/org/generation/italy/Main.java
@@ -0,0 +1,24 @@
+package org.generation.italy;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*public static void main(String[] args) {
+     HashSet<String> insiemeStudenti = new HashSet<>();
+	 insiemeStudenti.add(("Mario"));
+	 insiemeStudenti.add(("Pina"));
+	 insiemeStudenti.add(("Gino"));*/
+Scanner sc=new Scanner(System.in);
+float[] carte={1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
+		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
+		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
+		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f};
+
+
+System.out.println("");
+	}
+
+}

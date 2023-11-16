8d6d385008a733f2a3c1c3de5d63bf14c880a9b3
Cingolani03 <gabrielecingolani61@gmail.com>
2023-11-16 09:46:35 +0100

    secondo commit

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,5 +1,6 @@
 package org.generation.italy;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -12,13 +13,31 @@ public class Main {
 	 insiemeStudenti.add(("Pina"));
 	 insiemeStudenti.add(("Gino"));*/
 Scanner sc=new Scanner(System.in);
-float[] carte={1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
+float[] elencoCarte={1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
 		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
 		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
 		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f};
 
+String risposta;
+int i,carta=0,tot=0;
+Random r=new Random();
 
-System.out.println("");
+do
+{
+for(i=0;i<1;i++) {
+	carta=r.nextInt(elencoCarte.length);
+System.out.println("la carta pescata è: " + elencoCarte[carta]);
+}
+if(carta>7.5) {
+	System.out.println("Hai sballato");
+}	
+tot=tot+carta;
+System.out.println("la tua somma è: " + tot);
+System.out.println("vuoi pescare un'altra carta ?");
+risposta=sc.nextLine();
+}while(risposta.equals("si"));
+System.out.println("stai");
+System.out.println("la tua somma è: " + tot);
 	}
 
 }

cb90993005878a6f5de103360fb477fd6b74185a
Cingolani03 <gabrielecingolani61@gmail.com>
2023-11-16 12:04:51 +0100

    versione finale

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -17,7 +17,15 @@ float[] elencoCarte={1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
 		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
 		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
 		       1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f};
-
+String[]tipoCarte= {"asso di bastoni","2 di bastoni","3 di bastoni","4 di bastoni","5 di bastoni","6 di bastoni",
+		            "7 di bastoni","fante di bastoni","cavallo di bastoni","re di bastoni",
+		            "asso di spade","2 di spade","3 di spade","4 di spade","5 di spade","6 di spade",
+		            "7 di spade","fante di spade","cavallo di spade","re di spade",
+		            "asso di coppe","2 di coppe","3 di coppe","4 di coppe","5 di coppe","6 di coppe",
+		            "7 di coppe","fante di coppe","cavallo di coppe","re di coppe",
+		            "asso di denari","2 di denari","3 di denari","4 di denari","5 di denari","6 di denari",
+		            "7 di denari","fante di denari","cavallo di denari","re di denari"};
+	        
 String risposta;
 int i,carta=0;
 float conta=0f,tot=0;
@@ -27,9 +35,14 @@ Random r=new Random();
 do {
 do
 {
-
+	for(i=0;i<40;i++) {
+		
+	}
 carta=r.nextInt(elencoCarte.length);
-System.out.println("la carta pescata è: " + elencoCarte[carta]);
+System.out.println("la carta pescata è: " + tipoCarte[carta]);
+for(i=0;i<40;i++) {
+	
+}
 conta=elencoCarte[carta];
 tot=tot+conta;
 System.out.println("la tua somma è: " + tot);
@@ -44,9 +57,10 @@ System.out.println("vuoi pescare un'altra carta ?");
 risposta=sc.nextLine();
 }while(risposta.equals("si"));
 
-System.out.println("vuoi pescare un'altra carta ?");
+System.out.println("vuoi giocare ancora ?");
 risposta=sc.nextLine();
 }while(risposta.equals("si"));
+sc.close();
 	}
 
 }
